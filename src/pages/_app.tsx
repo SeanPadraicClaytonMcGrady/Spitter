69fdf6a1f25e4bf446b2a370af7971873221eadf
Sean Padraic Clayton McGrady <sean.padraic.clayton.mcgrady@gmail.com>
2023-06-16 10:05:26 -0300

    Feat(LogIn/LogOut)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import { type Session } from "next-auth";
+import React from "react";
+import Head from "next/head";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import SideNav from "~/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,7 +13,20 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Spitter</title>
+        <meta
+          name="description"
+          content="This is Twitter, but with algorithmic discussion."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex items-start sm:pr-4">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
