49e6bfd92b88ce310f8527d5c754af84b088e567
Sean Padraic Clayton McGrady <sean.padraic.clayton.mcgrady@gmail.com>
2023-06-27 11:25:35 -0300

    Feat:FeedButtons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,44 @@
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useState } from "react";
+import { number } from "zod";
 import { InfiniteSpitList } from "~/components/InfiniteSpitsList";
 import NewTweetForm from "~/components/NewSpitForm";
 import { api } from "~/utils/api";
 
 const TABS = ["Recent", "Following"] as const;
+
 const Home: NextPage = () => {
-  // const [selectedTab, setSelectedTab] =
-  //   useState<(typeof TABS)[number]>("Recent");
+  const [selectedTab, setSelectedTab] =
+    useState<(typeof TABS)[number]>("Recent");
+
   const session = useSession();
   return (
     <>
       <header className="sticky top-0 z-10 border-b bg-white pt-2">
         <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
         {session.status === "authenticated" && (
-          <div className="flex">{TABS}</div>
+          <div className="flex">
+            {TABS.map((tab) => {
+              return (
+                <button
+                  key={tab}
+                  className={`flex-grow p-2 hover:bg-gray-200 focus-visible:bg-gray-200 ${
+                    tab === selectedTab
+                      ? "border-b-4 border-b-blue-500 font-bold"
+                      : ""
+                  }`}
+                  onClick={() => setSelectedTab(tab)}
+                >
+                  {tab}
+                </button>
+              );
+            })}
+          </div>
         )}
       </header>
       <NewTweetForm />
-      <RecentSpits />
+      {selectedTab === "Recent" ? <RecentSpits /> : <FollowingSpits />}
     </>
   );
 };
@@ -27,6 +46,23 @@ const Home: NextPage = () => {
 function RecentSpits() {
   const spits = api.spit.infiniteFeed.useInfiniteQuery(
     {},
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
+
+  return (
+    <InfiniteSpitList
+      spits={spits.data?.pages.flatMap((page) => page.spits)}
+      isError={spits.isError}
+      isLoading={spits.isLoading}
+      hasMore={spits.hasNextPage || false}
+      fetchNewSpits={spits.fetchNextPage}
+    />
+  );
+}
+
+function FollowingSpits() {
+  const spits = api.spit.infiniteFeed.useInfiniteQuery(
+    { onlyFollowing: true },
     { getNextPageParam: (lastPage) => lastPage.nextCursor }
   );
 
