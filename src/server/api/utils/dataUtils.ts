680183ae8cf0d936235fe6585aec28b8a35d014e
Sean Padraic Clayton McGrady <sean.padraic.clayton.mcgrady@gmail.com>
2023-07-13 15:04:54 -0300

    Feature & Update: NLP modelling

--- src/server/api/utils/dataUtils.ts
+++ src/server/api/utils/dataUtils.ts
@@ -1,77 +1,24 @@
 import { prisma } from "~/server/db";
 import { Like, Dislike } from "@prisma/client";
 
-type MessageData = {
-  content: string;
-  liked: boolean;
-  disliked: boolean;
-  neutral: boolean;
-}[];
-
-export async function fetchTrainingMessageData(accountId: string) {
-  const likes: Like[] = await prisma.like.findMany({
-    where: {
-      userId: accountId,
-    },
-  });
-  const dislikes: Dislike[] = await prisma.dislike.findMany({
-    where: {
-      userId: accountId,
+export async function fetchLikedMessageTrainingData(
+  spitId: string,
+  userId: string
+) {
+  const likedMessage = await prisma.like.findUnique({
+    where: { userId_spitId: { userId, spitId } },
+    include: {
+      spit: true,
     },
   });
 
-  const likedSpitIds = likes.map((like) => like.spitId);
-  const dislikedSpitIds = dislikes.map((dislike) => dislike.spitId);
-  const likedSpits = await prisma.spit.findMany({
-    where: {
-      id: {
-        in: likedSpitIds,
-      },
-    },
-  });
+  const spitContent = likedMessage?.spit.content;
 
-  console.log(likedSpits, "liked spits");
+  if (spitContent === undefined) {
+    throw new Error(
+      "Unable to fetch message training data: spit content is undefined."
+    );
+  }
 
-  const notLikedSpits = await prisma.spit.findMany({
-    where: {
-      NOT: [
-        {
-          id: {
-            in: likedSpitIds,
-          },
-        },
-        {
-          id: {
-            in: dislikedSpitIds,
-          },
-        },
-      ],
-    },
-  });
-
-  const likedTrainingMessageData: MessageData = likedSpits.map((spit) => {
-    return {
-      content: spit.content,
-      liked: true,
-      disliked: false,
-      neutral: false,
-    };
-  });
-
-  const notLikedTrainingMessageData: MessageData = notLikedSpits.map((spit) => {
-    return {
-      content: spit.content,
-      liked: false,
-      disliked: false,
-      neutral: true,
-    };
-  });
-
-  console.log(likedTrainingMessageData, "Liked Training MessageData");
-
-  const trainingMessageData: MessageData = likedTrainingMessageData.concat(
-    notLikedTrainingMessageData
-  );
-
-  return trainingMessageData;
+  return spitContent;
 }
