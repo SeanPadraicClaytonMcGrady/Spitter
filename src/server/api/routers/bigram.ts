791ec3eec3b0d7e179ee87b25305f42fbd91dc22
Sean Padraic Clayton McGrady <sean.padraic.clayton.mcgrady@gmail.com>
2023-07-20 16:58:21 -0300

    Update: Bigram API

--- src/server/api/routers/bigram.ts
+++ src/server/api/routers/bigram.ts
@@ -1,31 +1,27 @@
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { z } from "zod";
+
+type featureCounts = {
+  [key: string]: number;
+};
+
+//This is the code I'm working on now.
 
 // export const bigramRouter = createTRPCRouter({
+//   updateBigrams: protectedProcedure
+//     .input(z.object({ featureCounts: z.record(z.number()) }))
+//     .mutation(async ({ input: { featureCounts }, ctx }) => {
+//       const currentUserId = await ctx.session?.user.id;
+//       const bigrams = Object.keys(featureCounts);
+//       const existingBigrams = await ctx.prisma.bigram.findMany({
+//         where: {
+//           bigram: {
+//             in: bigrams,
+//           }
+//         }
+//       })
+        
+//         return await void;
+//     }),
 
-//     updateBigrams: protectedProcedure.input(
-//         z.object({
-//             //Continue from here to update the table. Be careful: each user gets their own record of a bigram.
-//             //This is fine for now, but think of future efficiency.
-//         })
-//     )
-
-//     //This code is for reference as I am still learning.
-
-//     //  infiniteProfileFeed: publicProcedure
-//     // .input(
-//     //     z.object({
-//     //       userId: z.string(),
-//     //       limit: z.number().optional(),
-//     //       cursor: z.object({ id: z.string(), creationTime: z.date() }).optional(),
-//     //     })
-//     //   )
-//     //   .query(async ({ input: { limit = 10, userId, cursor }, ctx }) => {
-//     //     return await getInfiniteSpits({
-//     //       limit,
-//     //       ctx,
-//     //       cursor,
-//     //       whereClause: { userId },
-//     //     });
-//     //   }),
-
-// })
+});
