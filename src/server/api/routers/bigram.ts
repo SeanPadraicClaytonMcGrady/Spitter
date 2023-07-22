0147e17bd09fc2a08c1afbe04da85519cf827c7c
Sean Padraic Clayton McGrady <sean.padraic.clayton.mcgrady@gmail.com>
2023-07-22 13:26:10 -0300

    Feat: subtractBigrams route

--- src/server/api/routers/bigram.ts
+++ src/server/api/routers/bigram.ts
@@ -22,8 +22,10 @@ export const bigramRouter = createTRPCRouter({
       });
 
       for (const bigram of existingBigrams) {
-        const positiveCount = featureCounts[bigram.positiveCount] ?? 0;
-        const negativeCount = featureCounts[bigram.negativeCount] ?? 0;
+        const positiveCount =
+          (featureCounts[bigram.positiveCount] ?? 0) + bigram.positiveCount;
+        const negativeCount =
+          (featureCounts[bigram.negativeCount] ?? 0) + bigram.negativeCount;
         if (featureCounts[bigram.bigram]) {
           await ctx.prisma.bigram.update({
             where: { id: bigram.id },
@@ -43,7 +45,46 @@ export const bigramRouter = createTRPCRouter({
           });
         }
       }
+      const updateFinished = true;
+      return updateFinished;
+    }),
 
-      return { message: "Bigrams updated." };
+  subtractBigrams: protectedProcedure
+    .input(z.object({ featureCounts: z.record(z.number()) }))
+    .mutation(async ({ input: { featureCounts }, ctx }) => {
+      const currentUserId = ctx.session?.user.id;
+      const bigrams = Object.keys(featureCounts);
+      const existingBigrams = await ctx.prisma.bigram.findMany({
+        where: {
+          bigram: {
+            in: bigrams,
+          },
+        },
+      });
+
+      for (const bigram of existingBigrams) {
+        const positiveCount =
+          bigram.positiveCount - (featureCounts[bigram.positiveCount] ?? 0);
+        const negativeCount =
+          bigram.negativeCount - (featureCounts[bigram.negativeCount] ?? 0);
+        if (featureCounts[bigram.bigram]) {
+          await ctx.prisma.bigram.update({
+            where: { id: bigram.id },
+            data: {
+              positiveCount: featureCounts[bigram.positiveCount],
+              negativeCount: featureCounts[bigram.negativeCount],
+            },
+          });
+        } else if (!featureCounts[bigram.bigram]) {
+          await ctx.prisma.bigram.create({
+            data: {
+              bigram: bigram.bigram,
+              positiveCount: positiveCount,
+              negativeCount: negativeCount,
+              user: { connect: { id: currentUserId } },
+            },
+          });
+        }
+      }
     }),
 });
